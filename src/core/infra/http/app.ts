2e8537137dbd8aca422462ee621c5d70c7b2452a
Pedro Guilherme <pedroguilhermepedromen47@gmail.com>
2023-03-18 23:58:08 -0300

    refac: added cors stuff

--- src/core/infra/http/app.ts
+++ src/core/infra/http/app.ts
@@ -7,28 +7,17 @@ import { ensureAuthentication } from './middleware/ensureAuthentication'
 import { JWTService } from '~/core/services/jwt'
 import { container } from 'tsyringe'
 import { favoriteFeaturesRoutes } from './routes/features/favorites.routes'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001')
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-
-  // Pass to next layer of middleware
-  next()
-})
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 
 app.get('/', (req, res) => {
   const jwtService = container.resolve(JWTService)
